fe8f9aa4129d16e2d22f692452445f71a27500ca
scg990129@yahoo.com.hk <scg990129@yahoo.com.hk>
2026-03-20 14:01:35 -0700

    Add project files.

---
+++ PJTestMASM/Main.asm
@@ -0,0 +1,20 @@
+.386
+.model flat, stdcall
+.stack 4096
+
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+myList DWORD 2, 3, 5, 8
+my QWORD ?
+myy SQWORD ?
+
+.code
+main PROC
+  mov eax, 7
+  add eax, 8
+  INVOKE ExitProcess, eax
+main ENDP
+
+END main        ;specify the program's entry point
+# https://programminghaven.home.blog/2020/02/16/setup-an-assembly-project-on-visual-studio-2019/
